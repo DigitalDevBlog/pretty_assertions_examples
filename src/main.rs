bd73b8fe6b8273241f1014148c48a5953ed6c9c8
Digital Dev Blog <ruud@digitaldev.blog>
2025-01-19 17:27:56 +0100

    feat: added a test to compare maps

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ fn main() {
 mod test {
     use pretty_assertions::assert_eq as passert_eq;
     use pretty_assertions::assert_ne as passert_ne;
+    use std::collections::HashMap;
 
     /// This test should fail, because we are checking whether actual and expected are the same
     /// and they are not.
-   /// Remove the #[should_panic] line to see the results.
+    /// Remove the #[should_panic] line to see the results.
     #[test]
     #[should_panic]
     fn show_default_assert_eq() {
@@ -27,7 +28,7 @@ mod test {
     }
 
     /// This test should fail similar to the previous test with the standard assert.
-   /// Remove the #[should_panic] line to see the results.
+    /// Remove the #[should_panic] line to see the results.
     #[test]
     #[should_panic]
     fn show_default_pretty_assert_eq() {
@@ -36,9 +37,9 @@ mod test {
         passert_eq!(actual, expected);
     }
 
-   /// This test should fail because we expect both inputs to be different and they are not.
-   /// Remove the #[should_panic] line to see the results.
-   #[test]
+    /// This test should fail because we expect both inputs to be different and they are not.
+    /// Remove the #[should_panic] line to see the results.
+    #[test]
     #[should_panic]
     fn show_default_pretty_assert_ne() {
         let s1 = "Apple";
@@ -52,16 +53,31 @@ mod test {
     fn compare_multi_line_string() {
         let s1 = "This\nis\nmore\nthan\none\nline";
         let s2 = "Ths\nis\nnot\nmore\nthan\none\nline";
-        passert_eq!(s1,s2);
+        passert_eq!(s1, s2);
     }
 
     /// This test should fail. Remove the #[should_panic] line to see the results.
     #[test]
     #[should_panic]
     fn compare_data_structures() {
-        let v1 = vec![1,3,5,8];
-        let v2 = vec![1,4,6,8];
+        let v1 = vec![1, 3, 5, 8];
+        let v2 = vec![1, 4, 6, 8];
         passert_eq!(v1, v2);
     }
-}
 
+    /// This test should fail. Remove the #[should_panic] line to see the results.
+    #[test]
+    #[should_panic]
+    fn test_hashmap_equality() {
+        let mut map1 = HashMap::new();
+        map1.insert(1, "one");
+        map1.insert(2, "two");
+
+        let mut map2 = HashMap::new();
+        map2.insert(1, "one");
+        map2.insert(2, "twoo");
+
+        // They are identical, so this will pass silently
+        passert_eq!(map1, map2);
+    }
+}
